37e18000ac2a360cded4e7fe55bf67d983fd1498
phan.dang.hai.vu <SUN-ASTERISK\phan.dang.hai.vu@b121968-pc.sun-asterisk.com>
2019-11-15 10:11:29 +0700

    style: apply admin theme

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="clearfix">
+    <div class="pull-left">
+        <span>&copy; 2019 Chatword Fowarder. All rights reserved</span>
+    </div>
+    <div class="pull-right">
+        Developed by <a href="https://sun-asterisk.vn" target="_blank">Sun-asterisk</a>
+    </div>
+</footer>
