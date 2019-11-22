4e3c61a41fbd8e884cc5f1b26e4a8853f0405943
nguyen.van.thinhb <SUN-ASTERISK\nguyen.van.thinhb@i120938-pc.sun-asterisk.com>
2019-11-22 14:07:22 +0700

    Refs task #373683 Login test

---
+++ tests/Unit/LoginTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Auth;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_user_can_login_with_correct_credentials()
+    {
+        $password = '12345678';
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password),
+        ]);
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+        $response->assertRedirect('/');
+        $this->assertAuthenticatedAs($user);
+    }
+
+	public function test_user_cannot_login_with_incorrect_password()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt('12345678'),
+        ]);
+        
+        $response = $this->from('/login')->post('/login', [
+            'email' => $user->email,
+            'password' => 'invalid-password',
+        ]);
+        
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function test_remember_me_functionality()
+    {
+        $user = factory(User::class)->create([
+            'id' => random_int(1, 100),
+            'password' => bcrypt($password = '12345678'),
+        ]);
+        
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+            'remember' => 'on',
+        ]);
+        
+        $response->assertRedirect('/');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_logout()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password = '12345678'),
+        ]);
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+        $rp = $this->post('/logout', []);
+        $rp->assertRedirect('/login');
+        $this->assertGuest();
+    }
+
+    public function test_lock_user()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt('12345678'),
+        ]);
+        for ($i=0; $i < 7; $i++) { 
+            $response = $this->from('/login')->post('/login', [
+                'email' => $user->email,
+                'password' => 'invalid-password',
+            ]);
+        }
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+}
