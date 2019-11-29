a67085d11b8a4dd96b40a4f5e66cda9445735497
vovanquang12cntt <vanquang451@gmail.com>
2019-11-29 13:11:12 +0700

    feat: Bot Authorized

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Bot;
 use App\Models\Webhook;
+use App\Policies\BotPolicy;
 use App\Policies\WebhookPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -15,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
+        Bot::class => BotPolicy::class,
         Webhook::class => WebhookPolicy::class,
     ];
 
@@ -26,7 +29,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
     }
 }
