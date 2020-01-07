6c803b3feb8e2e6b00c9797455ac00b82ef1b819
phucqd <quach.dai.phuc@framgia.com>
2020-01-07 11:04:17 +0700

    Feat: update landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,26 +9,89 @@
     <title>Chatwork forwarder</title>
 </head>
 <body>
-    <div class="main">
-        <div class="logo">
-            <img src="{{asset('img/logo.png')}}" alt="">
-        </div>
-        <div class="content">
-            <h1>ABOUT SYSTEM</h1>
-            <p>Ứng dụng giúp các dự án có nhu cầu sử dụng chatwork api để gửi message hoặc muốn forward dữ liệu payload nhận được
-                từ các service khác lên chatwork.</p>
+    <div class="wrapper">
+        <div class="main">
+            <div class="logo">
+                <img src="{{asset('img/logo.png')}}" alt="">
+            </div>
+            <div class="content">
+                <h1>ABOUT SYSTEM</h1>
+                <p>The application assists the demand using API Chatwork of projects in sending messages or forwarding the payload data which is received from another service.</p>
 
-            @if(Auth::guest())
-            <a data-toggle="modal" href='#modal-login' class="btn btn-default">Login</a>
-            @else
-                @admin
-                <a href="admin/dashboard" class="btn btn-default">Go to app</a>
+                @if(Auth::guest())
+                    <a data-toggle="modal" href='#modal-login' class="btn btn-default">Login</a>
                 @else
-                <a href="/dashboard" class="btn btn-default">Go to app</a>
-                @endadmin
-            @endif
+                    @admin
+                    <a href="admin/dashboard" class="btn btn-default">Go to app</a>
+                    @else
+                        <a href="/dashboard" class="btn btn-default">Go to app</a>
+                        @endadmin
+                    @endif
+            </div>
         </div>
+        <div class="box-content">
+            <div class="title">
+                <span>features</span>
+            </div>
+            <div class="list-feature">
+                <div class="box-items">
+                    <div class="item">
+                        <h2>Statistic</h2>
+                        <img src="{{asset('img/cw-feature1.png')}}" alt="">
+                    </div>
+
+                    <div class="item">
+                        <h2>Webhook setting</h2>
+                        <img src="{{asset('img/cw-feature2.png')}}" alt="">
+                    </div>
+
+                    <div class="item">
+                        <h2>Payload and message history</h2>
+                        <img src="{{asset('img/cw-feature3.png')}}" alt="">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="box-content use-case">
+            <div class="title">
+                <span>Use case</span>
+            </div>
+            <div class="list-use-case">
+                <div class="box-items">
+                    <div class="item">
+                        <h2>Forward</h2>
+                        <p>Forward alert from Server/Application Monitoring service like Sentry, Updown, Grafana ... to Chatwork</p>
+                        <img src="{{asset('img/chatwork-icon.png')}}" alt="">
+                    </div>
+
+                    <div class="item">
+                        <h2>Notification</h2>
+                        <p>Send notification to Chatwork when there is a new Pull Request on Github </p>
+                        <img src="{{asset('img/github-icon.png')}}" alt="">
+                    </div>
+
+                    <div class="item">
+                        <h2>Contents</h2>
+                        <p>Integrate with other services that allow sending webhooks ...</p>
+                        <img src="{{asset('img/services.png')}}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer">
+            <div class="content-footer">
+                <p><span>Made by </span><a href="https://research.sun-asterisk.com/en"><img src="{{asset('img/Sun-logo.png')}}" alt=""></a> <span>R&D Lab</span></p>
+                <p>We love Open Source</p>
+                <p>This service is open-sourced at <a href="https://github.com/sun-asterisk-research/chatwork-forwarder"><img src="{{asset('img/github.png')}}" alt=""></a></p>
+            </div>
+        </div>
+
+        <img class="footer-logo" src="{{asset('img/cw-logo-footer.png')}}" alt="">
     </div>
+
     <div class="modal fade" id="modal-login">
         <div class="modal-dialog">
             <div class="login-form">
