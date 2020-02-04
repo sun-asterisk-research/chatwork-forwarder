99498d867fc1e26025a3ab32054d11b5c3085208
nguyen.van.thinhb <SUN-ASTERISKguyen.van.thinhb@i120938-pc.sun-asterisk.com>
2020-02-04 14:51:53 +0700

    Feat: show params content default

--- public/js/payload.js
+++ public/js/payload.js
@@ -259,4 +259,34 @@ $(document).ready(function () {
             });
         }
     });
+
+    $('#github').on('click', function (e) {
+        readTextFile("/json/template-params-github.json", function(text){
+            $("#payload_params").val(text);
+        });
+        readTextFile("/json/template-content-github.json", function(text){
+            $("#payload_content").val(text);
+        });
+    });
+
+    $('#viblo').on('click', function (e) {
+        readTextFile("/json/template-params-viblo.json", function(text){
+            $("#payload_params").val(text);
+        });
+        readTextFile("/json/template-content-viblo.json", function(text){
+            $("#payload_content").val(text);
+        });
+    });
+    function readTextFile(file, callback) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function () {
+            if (rawFile.readyState === 4 && rawFile.status == "200") {
+                callback(rawFile.responseText);
+            }
+        }
+        rawFile.send(null);
+    }
+
 });
