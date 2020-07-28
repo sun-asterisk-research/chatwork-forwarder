90e71399a1621b66f348f527f05404bba49cdb06
thinhbnv-0938 <SUN-ASTERISKguyen.van.thinhb@i120938-pc.sun-asterisk.com>
2020-07-28 13:41:11 +0700

    Test: mappingRepository

--- tests/Unit/Repositories/MappingRepositoryTest.php
+++ tests/Unit/Repositories/MappingRepositoryTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Repositories;
 
 use Tests\TestCase;
 use App\Models\Mapping;
+use App\Models\Webhook;
 use App\Models\User;
 use App\Repositories\Eloquents\MappingRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -38,5 +39,26 @@ class MappingRepositoryTest extends TestCase
         $mappingRepository->delete($mapping->id);
 
         $this->assertDatabaseMissing('mappings', ['id' => $mapping->id, 'deleted_at' => NULL]);
+    }
+
+    public function testGetKeys()
+    {
+        $mappingRepository = new MappingRepository;
+        $webhook = factory(Webhook::class)->create();
+        $mapping = factory(Mapping::class)->create(['webhook_id' => $webhook->id, 'key' => 'my key', 'value' => 'this value']);
+        $keys = $mappingRepository->getKeys($webhook);
+
+        $this->assertEquals('my key', $keys[0]);
+    }
+
+    public function testGetKeyAndValues()
+    {
+        $mappingRepository = new MappingRepository;
+        $webhook = factory(Webhook::class)->create();
+        $mapping = factory(Mapping::class)->create(['webhook_id' => $webhook->id, 'key' => 'my key', 'value' => 'this value']);
+        $keys = $mappingRepository->getKeyAndValues($webhook);
+
+        $this->assertEquals('my key', $keys[0]->key);
+        $this->assertEquals('this value', $keys[0]->value);
     }
 }
